1128cdf80c048e73ae7ff28134d83802b11fd445
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2024-12-23 17:32:17 +0530

    update

---
+++ MPMS/src/main/java/org/menter/application/mpms/AdminDashBoard.java
@@ -0,0 +1,12 @@
+package org.menter.application.mpms;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AdminDashBoard extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
